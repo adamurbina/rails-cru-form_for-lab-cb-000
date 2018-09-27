a1487fa15a2aa6b9355eee0808e6a0a9dcdcca2c
Adam Urbina <adamurbina@gmail.com>
2018-09-27 18:17:37 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,19 @@
 class ArtistsController < ApplicationController
+
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
   def show
     @artist = Artist.find(params[:id])
+  end
+
+  def new
+  end
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
   end
 end
