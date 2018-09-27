a1a7267419f38883c4ca6bc1d0a12df55ae86a30
Adam Urbina <adamurbina@gmail.com>
2018-09-27 18:25:11 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -16,6 +16,7 @@ class ArtistsController < ApplicationController
   def update
     @artist = Artist.find(params[:id])
     @artist.update(artist_params(:name, :bio))
+    redirect_to artist_path(@artist)
   end
 
   def artist_params(*args)
