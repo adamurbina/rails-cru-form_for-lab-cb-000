e2bf52a4eef8e5f159d8c3fd2faf8c1956955cd7
Adam Urbina <adamurbina@gmail.com>
2018-09-27 18:41:15 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,14 +1,19 @@
 class GenresController < ApplicationController
   def new
   end
-  
+
   def create
   end
 
   def show
+    @genre = Genre.find(params[:id])
   end
 
   def edit
   end
-  
+
+  def update
+    
+  end
+
 end
